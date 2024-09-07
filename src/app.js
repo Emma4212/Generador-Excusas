2e9433f8617122b0f7ebde2ba65416d3f5e235e7
Emma4212 <110198302+Emma4212@users.noreply.github.com>
2024-09-07 21:28:44 +0000

    Cambios 2.0

--- src/app.js
+++ src/app.js
@@ -6,30 +6,29 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-
-let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
-let action = ['ate', 'peed', 'crushed', 'broke'];
-let what = ['my homework', 'my phone', 'the car'];
-let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
-
-
   //write your code here
-let soyperra = document.getElementById("soyperra")
-soyperra.innerText = random();
-// console.log("Hello Rigo from the console!");
-
-function random (){
-
- let aleatorio =  Math.floor( Math.random( ))* when.length
- let aleatorioDos =  Math.floor( Math.random( ))* action.length
- let aleatorioTres =  Math.floor( Math.random( ))* who.length
- let aleatorioCuatro =  Math.floor( Math.random( ))* what.length
-
- let cadena = `${who[aleatorioTres]} ${action[aleatorioDos]} ${when[aleatorio]} ${what[aleatorioCuatro]}` 
-return cadena;
-
-
-}
-
-
+  let soyperra = document.getElementById("soyperra");
+  soyperra.innerText = random();
+  // console.log("Hello Rigo from the console!");
 };
+
+let who = ["The dog", "My grandma", "The mailman", "My bird"];
+let action = ["ate", "peed", "crushed", "broke"];
+let what = ["my homework", "my phone", "the car"];
+let when = [
+  "before the class",
+  "when I was sleeping",
+  "while I was exercising",
+  "during my lunch",
+  "while I was praying"
+];
+
+function random() {
+  let aleatorio = Math.floor(Math.random() * when.length);
+  let aleatorioDos = Math.floor(Math.random() * action.length);
+  let aleatorioTres = Math.floor(Math.random() * who.length);
+  let aleatorioCuatro = Math.floor(Math.random() * what.length);
+
+  let cadena = `${who[aleatorioTres]} ${action[aleatorioDos]} ${when[aleatorio]} ${what[aleatorioCuatro]}`;
+  return cadena;
+}
